ecbcbdc16171f71b42db9447594f58ddf3060736
Jaap van Leeuwen <73707148+JaapvanLeeuwen@users.noreply.github.com>
2021-05-05 14:12:32 +0200

    Added files (javasource etc.) to folder

---
+++ javasource/geodistancecalculator/actions/ValidateGeoDistanceCalculatorInput.java
@@ -0,0 +1,13 @@
+package geodistancecalculator.actions;
+
+import java.math.BigDecimal;
+
+public class ValidateGeoDistanceCalculatorInput {
+	
+	public void validateValues(BigDecimal lat1, BigDecimal lng1, BigDecimal lat2, BigDecimal lng2, BigDecimal earthRadius) {
+		
+		if (lat1 == null || lng1 == null || lat2 == null || lng2 == null || earthRadius == null) {
+			throw new GeolocationInputValidationException("One or multiple values are empty. Please ensure all parameters are configured.");
+		}
+	}
+}
